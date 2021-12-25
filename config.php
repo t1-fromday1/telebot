17b9c31d8b95f307dc0c30c21075f47f2a1af1ad
Toon <79970016+t1-fromday1@users.noreply.github.com>
2021-12-26 00:25:52 +0300

    add api_key

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    $API_KEY = "5080289828:AAEq5SYKmiDqnjyMUdXS-YmwgZa3ePMy26Y";
+
+    // images
+    $client_image = "https://wearegenerationshapers.co.ke/pandebot/images/pande.jpg";
+    $inclusion = "https://wearegenerationshapers.co.ke/pandebot/images/inclusion.jpg";
+    $participation = "https://wearegenerationshapers.co.ke/pandebot/images/participation.jpg";
+    $youths = "https://wearegenerationshapers.co.ke/pandebot/images/youths.jpg";
+    $leadership = "https://wearegenerationshapers.co.ke/pandebot/images/leadership.jpg";
+    $women = "https://wearegenerationshapers.co.ke/pandebot/images/women.jpg";
+?>
